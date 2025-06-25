fc8c06f680555eb6a30eb26626fdc7f03fe8b108
Naeim <naeim.smz@gmail.com>
2025-06-25 21:51:54 +0430

    handling click events

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct number!';
 
@@ -8,3 +9,13 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 11;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
