f3ddf8704b3be7f92491f55dff467d7f92da7cb7
Naeim <naeim.smz@gmail.com>
2025-06-25 20:07:11 +0430

    selecting and manipulating elements

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct number!';
+
+document.querySelector('.number').textContent = 9;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 11;
+console.log(document.querySelector('.guess').value);
